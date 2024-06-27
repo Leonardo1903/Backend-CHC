35a45ba5d853bbecbb269cae16dba0a9f3ed1c55
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2024-06-27 10:16:15 +0530

    add user credentials update controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -214,3 +214,147 @@ export const refreshAccessToken = asyncHandler(async (req, res) => {
     throw new apiError(401, "Unauthorized request");
   }
 });
+
+export const changePassword = asyncHandler(async (req, res) => {
+  // User change password logic:-
+  // Get user details from frontend
+  // validate user details - not empty, valid email, password length
+  // Check if user exists: email
+  // Check for old password
+  // Update password
+  // Send response
+
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user._id);
+
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new apiError(401, "Invalid old password");
+
+    if (oldPassword === newPassword) {
+      throw new apiError(
+        400,
+        "New password cannot be the same as old password"
+      );
+    }
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, "Password changed successfully"));
+});
+
+export const getUserProfile = asyncHandler(async (req, res) => {
+  // User get profile logic:-
+  // Get user details from frontend
+  // Check if user exists: email
+  // Send response
+
+  const user = await User.findById(req.user._id).select("-password");
+
+  if (!user) {
+    throw new apiError(404, "User not found");
+  }
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, "User profile fetched successfully", user));
+});
+
+export const updateUserProfile = asyncHandler(async (req, res) => {
+  // User update profile logic:-
+  // Get user details from frontend
+  // Check if user exists: email
+  // Update user object - update entry in database
+  // Remove password and refresh token from response
+  // Send response
+
+  const { fullName, email, username } = req.body;
+
+  if ([fullName, email, username].some((field) => field?.trim() === "")) {
+    throw new apiError(400, "All fields are required");
+  }
+
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    throw new apiError(404, "User not found");
+  }
+
+  user.fullName = fullName;
+  user.email = email;
+  user.username = username;
+
+  await user.save({ validateBeforeSave: false });
+
+  const updatedUser = await User.findByIdAndUpdate(
+    user._id,
+    {
+      $set: { fullName, email, username },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(
+      new apiResponse(200, "User profile updated successfully", updatedUser)
+    );
+});
+
+export const updateAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.files.avatar[0].path;
+
+  if (!avatarLocalPath) {
+    throw new apiError(400, "Avatar is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  if (!avatar.url) {
+    throw new apiError(400, "Error uploading avatar");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: { avatar: avatar.url },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, "Avatar updated successfully", user));
+});
+
+export const updateCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.files.avatar[0].path;
+
+  if (!coverImageLocalPath) {
+    throw new apiError(400, "Cover Image is required");
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!coverImage.url) {
+    throw new apiError(400, "Error uploading cover image");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: { coverImage: coverImage.url },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, "Cover Image updated successfully", user));
+});
