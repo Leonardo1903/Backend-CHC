c8c4a321ced80f72eeca887e17c53df38b7e9889
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2025-04-29 15:20:36 +0530

    feat(logging): integrate morgan and winston for enhanced logging

--- Main/src/app.js
+++ Main/src/app.js
@@ -1,8 +1,28 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import logger from "./utils/logger.js";
+import morgan from "morgan";
+
+const morganFormat = ":method :url :status :response-time ms";
 
 const app = express();
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 app.use(
   cors({
